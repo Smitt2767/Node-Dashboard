1452c562a9b4559744bb590b4cda57159a268bf2
smit-vekariya-digitattva <smit.vekariya@digitattva.com>
2021-07-28 20:49:12 +0530

    chat functionality added

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const fileUpload = require("express-fileupload");
-
+const socketIo = require("socket.io");
 const PORT = process.env.PORT || 3001;
 
 // routes
 const ckEditorRoute = require("./routes/ckEditor");
 const signatureRoute = require("./routes/signature");
+
+// services
+const { addUser, removeUser } = require("./services/userService");
 
 app.use(express.json({ limit: 500000 }));
 app.use(express.static("public"));
@@ -15,4 +18,36 @@ app.use(fileUpload());
 app.use("/ck", ckEditorRoute);
 app.use("/signature", signatureRoute);
 
-app.listen(PORT, console.log(`server is running on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`server is running on port ${PORT}`)
+);
+
+const io = socketIo(server, {
+  origin: "*",
+  methods: ["GET", "POST"],
+});
+
+io.on("connection", (socket) => {
+  socket.on("joinChatRoom", (username) => {
+    addUser({ username, id: socket.id });
+    socket.join("chat");
+    socket.to("chat").emit("message", {
+      message: `${username} is joined!`,
+      by: "admin",
+    });
+  });
+
+  socket.on("message", (data) => {
+    socket.to("chat").emit("message", data);
+  });
+
+  socket.on("typing", (data) => {
+    socket.to("chat").emit("typing", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+    removeUser(socket.id);
+  });
+});
---
+++ services/userService.js
@@ -0,0 +1,17 @@
+let users = [];
+
+exports.addUser = ({ username, id }) => {
+  users.push({ username, id });
+};
+
+exports.getUsers = () => {
+  return users;
+};
+
+exports.getUserById = (id) => {
+  return users.find((user) => (user.id = id));
+};
+
+exports.removeUser = (id) => {
+  users = users.filter((user) => user.id !== id);
+};
